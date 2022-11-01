1bf7a6200bff606a68a38ea5ef3ab6f01b18049b
aVariantOfMine <117154310+aVariantOfMine@users.noreply.github.com>
2022-11-01 16:47:35 +0530

    uploaded files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName("mainbox").style.diaplay = "none";
+
+function inProcess(){
+	alert("sorry! currently you are unable to use this button!");
+}
+function redirect(){
+	alert("you will be redirected to typeracer.com")
+}
